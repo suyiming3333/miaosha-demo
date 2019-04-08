91fad810b37ca187ef6d8c9a875ac5a8629e4fe9
suyiming <suyiming3333@gmail.com>
2019-04-09 00:31:17 +0800

    demo-init

---
+++ src/main/java/com/sym/miaoshaodemo/MiaoshaoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sym.miaoshaodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoshaoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoshaoDemoApplication.class, args);
+    }
+
+}
